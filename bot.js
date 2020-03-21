85c98f9c5c36d4d1b2111fbfef41da8a9001b01b
Gamebot3 <ansh.jain1@outlook.com>
2020-03-21 18:44:17 -0500

    Parker

--- bot.js
+++ bot.js
@@ -65,6 +65,7 @@ client.on('message', message => {
     }
     if (command === `ansh`) {
         message.channel.send('Justin is a dumbass.');
+        message.channel.send('Parker is not a dumbass.');
     }
     if (command === `headout`) {
         const attachment = new Discord.MessageAttachment('https://anshjainpublic.s3.us-east-2.amazonaws.com/headout.gif');
--- ec2-user@ec2-54-162-247-164.compute-1.amazonaws.com
+++
@@ -1,4 +0,0 @@
-{
-	"prefix": "+",
-	"token": "Njg4OTY3MjQxODUyNTE4NDYx.Xm8K-A.6wPpOgLythV4x7IJ-WlwQ7LuERQ"
-}
